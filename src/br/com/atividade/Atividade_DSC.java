b7d3f8942d4114484ffa954e958360defe975590
moize <moize@10.112.4.243>
2019-03-15 19:38:25 -0300

    Commit TESTE

--- src/br/com/atividade/Atividade_DSC.java
+++ src/br/com/atividade/Atividade_DSC.java
@@ -7,7 +7,7 @@ package br.com.atividade;
  */
 public class Atividade_DSC {
 	public static void main(String[] args) {
-		System.out.println("Criando um projeto java e commitado através do Eclise");
+		System.out.println("Criando um projeto java e commitado através do Eclise - TESTE");
 			
 	}
 	
